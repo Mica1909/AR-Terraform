28f617084541362a92fbe81ee8cbafb8e0969a91
Mica1909 <mikegoitia11@outlook.com>
2024-02-29 14:10:32 +0000

    test

---
+++ modules/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_virtual_machine_extension" "web_server_install" {
+  name                       = "${var.prefix}-wsi"
+  virtual_machine_id         = var.virtual_machine_id
+  publisher                  = var.publisher
+  type                       = var.type
+  type_handler_version       = var.type_handler_version
+  auto_upgrade_minor_version = var.auto_upgrade_minor_version
+
+  settings = var.extension_settings
+}
