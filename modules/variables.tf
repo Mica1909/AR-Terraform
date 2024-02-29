ffbb3b4a47d6acc6a52f9541e558f4a4bce820d4
Mica1909 <145031324+Mica1909@users.noreply.github.com>
2024-02-29 11:17:30 -0300

    test

---
+++ modules/variables.tf
@@ -0,0 +1,18 @@
+
+
+
+variable "vm_name" {
+  description = "Nombre de la máquina virtual"
+}
+
+variable "vm_resource_group_name" {
+  description = "Nombre del grupo de recursos de la máquina virtual"
+}
+
+variable "vm_location" {
+  description = "Ubicación de la máquina virtual"
+}
+
+variable "vm_id" {
+  description = "ID de la máquina virtual"
+}
